d4c9cc98e587f4e4c321c6fd45b932bb023c4038
ChipuChipu <Joshuay.yi@hotmail.com>
2018-08-04 07:02:40 -0700

    Added Combined Code
    Issue with Delegate for Listing S3 Objects.

---
+++ Assets/Josh's Folder/Asset Scripts/Driver.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Amazon;
+using AssetStruct;
+
+public class Driver : MonoBehaviour 
+{
+	void Start()
+	{
+		UnityInitializer.AttachToGameObject (this.gameObject);
+		AWSConfigs.HttpClient = AWSConfigs.HttpClientOption.UnityWebRequest;
+
+		// Initialize S3's FileList
+		S3AssetStructure.LoadFiles();
+		// Initialize Local FileList
+		LocalAssetStructure.LoadInitialFiles();
+	}
+
+	void Update()
+	{
+		if (Input.GetKey ("up")) {
+			LocalAssetStructure.CompareDictionaries (S3AssetStructure.GetS3FileList ());
+		}
+			
+		else if (Input.GetKey("down"))
+			// Download Necessary Files from S3
+			S3AssetStructure.S3GetObjects(LocalAssetStructure.GetModifiedFileList());
+	}
+}
