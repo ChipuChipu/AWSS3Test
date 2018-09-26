33fb3108782c5ac730fbd4969f22afd95a5508ac
George <gpquanli@hotmail.com>
2018-09-26 14:23:06 -0700

    First commit.

---
+++ Assets/StreamingModule/George Folder/Download Testing/ShowString.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEngine.UI;
+
+public class ShowString : MonoBehaviour {
+
+	public Text text;
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Directory.Exists (LocalAssetLoader.DirectoryPath)) {
+			string[] files = Directory.GetFiles (LocalAssetLoader.DirectoryPath);
+			string completeString = "";
+			foreach (var txt in files) {
+				completeString += txt + "\n";
+			}
+			text.text = completeString;
+		}
+
+	}
+}
